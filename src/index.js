6d957ee9846cf8a81f95bb567bd7262df50aa02f
DarioFunes89 <dariofunes89@gmail.com>
2023-12-13 11:04:33 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import app from './app.js'
+import {sequelize} from './database/database.js'
 const PORT = 3001;
 
-app.listen(PORT)
-console.log(`'Server is listening on port: ${PORT}'`);
+import './models/Stock.js'
+
+async function main() {
+    try {
+        await sequelize.sync({force:true})
+        app.listen(PORT)
+        console.log(`'Server is listening on port: ${PORT}'`);
+    } catch (error) {
+        console.error('Unable to connect to the database', error);
+    }
+}
+
+main();
